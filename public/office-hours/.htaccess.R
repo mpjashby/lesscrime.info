2ea77373f8b364cfd43672cebae23d3e89b8b244
mpjashby <mpjashby@gmail.com>
2021-09-28 12:54:47 +0100

    Revise office-hours htaccess

--- public/office-hours/.htaccess.R
+++ public/office-hours/.htaccess.R
@@ -1,3 +1,3 @@
-# Redirect directory to MS Bookings site
+# Redirect directory to Microsoft Bookings site
 # Redirect code from https://mediatemple.net/community/products/all/204643080/how-do-i-redirect-my-site-using-a-.htaccess-file
 Redirect 301 / https://outlook.office365.com/owa/calendar/UCL813760@ucl.ac.uk/bookings/
