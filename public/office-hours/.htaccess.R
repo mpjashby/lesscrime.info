80e6e198ebb731de8d78b34d57a4d54892a94a04
mpjashby <mpjashby@gmail.com>
2021-12-06 16:04:59 +0000

    Add stop and search report for 2021 Q3

--- public/office-hours/.htaccess.R
+++
@@ -1,3 +0,0 @@
-# Redirect directory to Microsoft Bookings site
-# Redirect code from https://mediatemple.net/community/products/all/204643080/how-do-i-redirect-my-site-using-a-.htaccess-file
-Redirect 301 / https://outlook.office365.com/owa/calendar/UCL813760@ucl.ac.uk/bookings/
